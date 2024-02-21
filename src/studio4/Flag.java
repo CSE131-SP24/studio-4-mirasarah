72077911cb6b9e63409216c248aca0e082cab657
mirarosovsky <mirarosovsky@localhost>
2024-02-21 15:44:58 -0600

    :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,31 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.rectangle(.5, .5, .4, .3);
+		StdDraw.setPenColor(Color.yellow);
+		StdDraw.filledRectangle(.5, .5, .4, .3);
+		
+		StdDraw.setPenColor(Color.pink);
+		StdDraw.filledRectangle(.5, .5, .35, .25);
+		
+		StdDraw.setPenColor(Color.yellow);
+		StdDraw.filledCircle(.5, .5, .15);
+		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.ellipse(.5, .47, .09, .075);
+		
+		StdDraw.setPenColor(Color.yellow);
+		StdDraw.filledRectangle(.5, .5, .1, .05);
+		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledEllipse(.45, .55, .01, .01);
+		
+		StdDraw.setPenColor(Color.black);
+		StdDraw.filledEllipse(.55, .55, .01, .01);
+		
+		StdDraw.text(.5, .7, "BE HAPPY");
+		StdDraw.text(.5, .3, "BITCHES");
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,71 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		if (shape.equals("ellipse")) {
+			double centerX = in.nextDouble();
+			double centerY = in.nextDouble();
+			double halfLength = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			if (filled == false) {
+				Color shade = new Color(red, green, blue);
+				StdDraw.setPenColor(shade);
+				StdDraw.ellipse(centerX, centerY, halfWidth, halfLength);
+			}
+			else {
+				Color shade = new Color(red, green, blue);
+				StdDraw.setPenColor(shade);
+				StdDraw.filledEllipse(centerX, centerY, halfWidth, halfLength);
+
+			}
+		}
+		if (shape.equals("rectangle")) {
+			double centerX = in.nextDouble();
+			double centerY = in.nextDouble();
+			double halfLength = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			if (filled == false) {
+				Color shade = new Color(red, green, blue);
+				StdDraw.setPenColor(shade);
+				StdDraw.rectangle(centerX, centerY, halfWidth, halfLength);
+			}
+			else {
+				Color shade = new Color(red, green, blue);
+				StdDraw.setPenColor(shade);
+				StdDraw.filledRectangle(centerX, centerY, halfWidth, halfLength);
+
+			}
+		}
+		if	(shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			Color shade = new Color(red, green, blue);
+			StdDraw.setPenColor(shade);
+			double[] x = { x1, x2, x3};
+			double[] y = {y1, y2, y3};
+			if(filled == false){
+				StdDraw.polygon(x, y);
+			}
+			else {
+				StdDraw.filledPolygon(x, y);
+
+
+
+			}	
+
+		}
+
+
+
 	}
+
 }
